42fe9ff47ec6a521d804da7e80544ce89725217f
hristo-darlyanov <memeda963@gmail.com>
2023-03-29 15:46:18 +0300

    Created structure for ServiceProduct.cs

---
+++ ShoppingSystem/ServiceProduct.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingSystem
+{
+    internal class ServiceProduct : Product
+    {
+        private double time;
+
+        public ServiceProduct(string name, double price, double time) : base(name, price)
+        {
+            Time = time;
+        }
+
+        public double Time { get => time; set => time = value; }
+
+        public override string ToString()
+        {
+            return "";
+        }
+    }
+}
