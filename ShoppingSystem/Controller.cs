2a16cb6c75c3252cbb4a95c9e688614b690de13a
hristo-darlyanov <memeda963@gmail.com>
2023-03-29 15:53:55 +0300

    Created structure for Controller.cs

---
+++ ShoppingSystem/Controller.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingSystem
+{
+    internal class Controller
+    {
+        public string ProcessProductCommand(List<string> args)
+        {
+            //TODO: Add some logic here …
+            throw new NotImplementedException();
+        }
+
+        public string ProcessServiceCommand(List<string> args)
+        {
+            //TODO: Add some logic here …
+            throw new NotImplementedException();
+        }
+
+        public string ProcessCheckoutCommand(List<string> args)
+        {
+            //TODO: Add some logic here …
+            throw new NotImplementedException();
+        }
+
+        public string ProcessInfoCommand(List<string> args)
+        {
+            //TODO: Add some logic here …
+            throw new NotImplementedException();
+        }
+
+        public string ProcessEndCommand()
+        {
+            //TODO: Add some logic here …
+            throw new NotImplementedException();
+        }
+    }
+}
