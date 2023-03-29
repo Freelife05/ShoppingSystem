e1005120f3f71cd7542462c295c4aee26baec73a
hristo-darlyanov <memeda963@gmail.com>
2023-03-29 15:39:58 +0300

    Created structure for Product.cs

---
+++ ShoppingSystem/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingSystem
+{
+    internal class Product
+    {
+        private string name;
+        private double price;
+
+        public string Name { get => name; set => name = value; }
+        public double Price { get => price; set => price = value; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return "";
+        }
+    }
+}
