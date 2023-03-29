2f7decfc183d322b48b18e7682a3df7a41439b86
hristo-darlyanov <memeda963@gmail.com>
2023-03-29 15:50:39 +0300

    Added structure for Receipt.cs

--- ShoppingSystem/PhysicalProduct.cs
+++ ShoppingSystem/PhysicalProduct.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace ShoppingSystem
 {
-    internal class PhysicalProduct
+    internal class PhysicalProduct : Product
     {
+        private double weight;
+
+        public double Weight { get => weight; set => weight = value; }
+
+        public PhysicalProduct(string name, double price, double weight) : base(name, price)
+        {
+            Weight = weight; 
+        }
+
+        public override string ToString()
+        {
+            return "";
+        }
     }
 }
---
+++ ShoppingSystem/Receipt.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingSystem
+{
+    internal class Receipt
+    {
+        private string customerName;
+
+        public Receipt(string customerName)
+        {
+            CustomerName = customerName;
+        }
+
+        public string CustomerName { get => customerName; set => customerName = value; }
+
+        public void AddProduct(Product product)
+        {
+            
+        }
+
+        public override string ToString()
+        {
+            return "";
+        }
+    }
+}
