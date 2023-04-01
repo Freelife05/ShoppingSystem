f34391a22652c49680f95cdc1a952ab5b0fe2a32
Freelife05 <83334140+Freelife05@users.noreply.github.com>
2023-04-01 22:07:02 +0300

    Updated Product and PhysicalProduct

--- ShoppingSystem/PhysicalProduct.cs
+++ ShoppingSystem/PhysicalProduct.cs
@@ -3,23 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace ShoppingSystem
 {
     internal class PhysicalProduct : Product
     {
+        private string name;
+        private double price;
         private double weight;
 
+        public string Name { get => name; set => name = value; }
+        public double Price { get => price; set => price = value; }
         public double Weight { get => weight; set => weight = value; }
 
         public PhysicalProduct(string name, double price, double weight) : base(name, price)
         {
-            Weight = weight; 
+            Weight = weight;
+            if (Weight <= 0)
+            {
+                throw new ArgumentException(String.Format("Weight cannot be less or equal to 0!"));
+            }
+
+            if (name.Length < 3 && name.Length > 30)
+            {
+                throw new ArgumentException(String.Format("Name should be between 3 and 30 characters!"));
+            }
+            if (Price < 0)
+            {
+                throw new ArgumentException(String.Format("Price should be 0 or positive!"));
+            }
         }
 
         public override string ToString()
         {
-            return "";
+            return $"The product's Name is {Name}, its price is {Price} and its weight is {weight}";
         }
     }
 }
--- ShoppingSystem/Product.cs
+++ ShoppingSystem/Product.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace ShoppingSystem
 {
@@ -14,15 +15,25 @@ namespace ShoppingSystem
         public string Name { get => name; set => name = value; }
         public double Price { get => price; set => price = value; }
 
+
         protected Product(string name, double price)
         {
             Name = name;
             Price = price;
+
+            if (name.Length < 3 && name.Length > 30)
+            {
+                throw new ArgumentException(String.Format("Name should be between 3 and 30 characters!"));
+            }
+            if (Price < 0)
+            {
+                throw new ArgumentException(String.Format("Price should be 0 or positive!"));
+            }
         }
 
         public override string ToString()
         {
-            return "";
+            return $"The Product's name is {name}, and it costs{price}";
         }
     }
 }
