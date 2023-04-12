a72f0f2247e543bad13f18e2f8ce56fdf02897ee
Preslav "Artrita" Ivanov <dimcho05@abv.bg>
2023-04-12 15:24:41 +0300

    Added logic to all the methods in controller
    Pretty much added logic to Product,Service,Checkout,Info and End

--- ShoppingSystem/Controller.cs
+++ ShoppingSystem/Controller.cs
@@ -8,34 +8,70 @@ namespace ShoppingSystem
 {
     internal class Controller
     {
+        private List<Product> products = new List<Product>();
+        private List<Receipt> receipts = new List<Receipt>();
+
         public string Product(List<string> args)
         {
-            //TODO: Add some logic here …
-            throw new NotImplementedException();
+            string name = args[0];
+            double price = double.Parse(args[1]);
+            double weight = double.Parse(args[2]);
+            products.Add(new PhysicalProduct(name, price, weight));
+            return $"Created new Physical Product";
         }
 
         public string Service(List<string> args)
         {
-            //TODO: Add some logic here …
-            throw new NotImplementedException();
+            string name = args[0];
+            double price = double.Parse(args[1]);
+            double weight = double.Parse(args[2]);
+            products.Add(new ServiceProduct(name, price, weight));
+            return $"Created new Service Product";
         }
 
         public string Checkout(List<string> args)
         {
-            //TODO: Add some logic here …
-            throw new NotImplementedException();
+            Receipt receipt = new Receipt(args[0]);
+            foreach (var item in products)
+            {
+                receipt.AddProduct(item);
+            }
+            receipts.Add(receipt);
+            products.Clear();
+            return "Checked out";
         }
 
         public string Info(List<string> args)
         {
-            //TODO: Add some logic here …
-            throw new NotImplementedException();
+            string info = args[0];
+            string fullInfo = "";
+            if (info == "Customer")
+            {
+                foreach (var item in products)
+                {
+                    fullInfo += $"{item.ToString()}\n";
+                }
+                return fullInfo;
+            }
+            else if (info == "Shop")
+            {
+                foreach (var item in receipts)
+                {
+                    fullInfo += $"{item.ToString()}\n";
+                }
+                return fullInfo;
+            }
+            else
+            {
+                return "Invalid Input";
+            }
         }
 
         public string End()
         {
-            //TODO: Add some logic here …
-            throw new NotImplementedException();
+            Console.WriteLine($"Amount of customers; {receipts.Count}");
+            Environment.Exit(0);
+            return "";
         }
     }
 }
