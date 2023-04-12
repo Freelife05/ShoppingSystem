6df40a51fb117ab9aa88289842001a4ddcffa2d4
Freelife05 <83334140+Freelife05@users.noreply.github.com>
2023-04-12 14:24:27 +0300

    Fixed Physical Product

--- ShoppingSystem/PhysicalProduct.cs
+++ ShoppingSystem/PhysicalProduct.cs
@@ -11,29 +11,33 @@ namespace ShoppingSystem
     {
         private double weight;
 
-        public double Weight { get => weight; set => weight = value; }
+        public double Weight 
+        {
+            get
+            {
+                return weight;
+            }
+            set
+            {
+                if (Weight <= 0)
+                {
+                    throw new ArgumentException(String.Format("Weight cannot be less or equal to 0!"));
+                }
+                else
+                {
+                    this.weight = value;
+                }
+            }
+        }
 
         public PhysicalProduct(string name, double price, double weight) : base(name, price)
         {
             Weight = weight;
-            if (Weight <= 0)
-            {
-                throw new ArgumentException(String.Format("Weight cannot be less or equal to 0!"));
-            }
-
-            if (name.Length < 3 && name.Length > 30)
-            {
-                throw new ArgumentException(String.Format("Name should be between 3 and 30 characters!"));
-            }
-            if (Price < 0)
-            {
-                throw new ArgumentException(String.Format("Price should be 0 or positive!"));
-            }
         }
 
         public override string ToString()
         {
-            return $"The product's Name is {Name}, its price is {Price} and its weight is {weight}";
+            return String.Format($"The product's Name is {Name}, its price is {Price} and its weight is {weight}");
         }
     }
 }
