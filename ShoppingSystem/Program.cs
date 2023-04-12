49e4ab0c0f84b8ec9e414733865c247654a86d1d
Preslav "Artrita" Ivanov <dimcho05@abv.bg>
2023-04-12 15:39:48 +0300

    Added the whole system
    Added the whole system for the program.cs to work and nearlly died.

--- ShoppingSystem/Program.cs
+++ ShoppingSystem/Program.cs
@@ -10,7 +10,45 @@ namespace ShoppingSystem
     {
         static void Main(string[] args)
         {
+            Controller controller = new Controller();
+            while (true)
+            {
+                var splittedInput = Console.ReadLine().Split();
+                string command = splittedInput[0];
+                List<string> arguments = splittedInput
+                    .Skip(1)
+                    .ToList();
 
+                try
+                {
+                    switch (command)
+                    {
+                        case "Product":
+                            Console.WriteLine(controller.Product(arguments));
+                            break;
+                        case "Service":
+                            Console.WriteLine(controller.Service(arguments));
+                            break;
+                        case "Checkout":
+                            Console.WriteLine(controller.Checkout(arguments));
+                            break;
+                        case "Info":
+                            Console.WriteLine(controller.Info(arguments));
+                            break;
+                        case "End":
+                            Console.WriteLine(controller.End());
+                            break;
+                        default:
+                            Console.WriteLine("Invalid command");
+                            break;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
         }
     }
 }
+ 
